8391b6b2df02b0e9718d1aeb8fe673362dd8a699
Sharif Md. Rakibul Raihan <92089362+smrraihan@users.noreply.github.com>
2026-06-15 10:59:32 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from PIL import Image
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("HishabAI MVP")
+
+uploaded_file = st.file_uploader(
+    "Upload bKash Screenshot",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file:
+
+    image = Image.open(uploaded_file)
+
+    st.image(image)
+
+    if st.button("Extract Transaction"):
+
+        prompt = """
+        Read this payment screenshot.
+
+        Extract:
+
+        - Transaction Type
+        - Merchant Name
+        - Amount
+
+        Return only plain text.
+        """
+
+        response = model.generate_content(
+            [prompt, image]
+        )
+
+        st.write(response.text)
