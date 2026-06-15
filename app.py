c7070ae76c5b2ac6f7043d0fdc6195fcaa7963ea
Sharif Md. Rakibul Raihan <92089362+smrraihan@users.noreply.github.com>
2026-06-15 17:37:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
-
+st.image("hishabai_logo.png", width=150)
 st.title("hishabAI MVP")
 
 uploaded_file = st.file_uploader(
